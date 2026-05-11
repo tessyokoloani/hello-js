01f446971f645ef20d658ef4859bc2655c7a7925
Tessy <tessyokoloani@gmail.com>
2026-05-11 23:57:57 +0100

    First assignment

---
+++ intro.js
@@ -0,0 +1,14 @@
+
+const myDetails = {
+    "name":"Tessy Okoloani",
+    "age":42,
+    "favoriteProgrammingLanguage":"Javascript"
+}
+
+
+
+console.log({
+    "name": myDetails.name, 
+    "age": myDetails.age, 
+    "favorite programming language": myDetails.favoriteProgrammingLanguage
+})
